8fe90307cee024485b31efa71b8f36cf39895cfa
Gerald Schneider <gerald@schneidr.de>
2024-07-20 17:32:37 +0200

    finished day02-1

--- day02-1/src/main.rs
+++ day02-1/src/main.rs
@@ -3,29 +3,38 @@ use std::fs::read_to_string;
 
 // https://adventofcode.com/2023/day/2
 
-let mut total_cubes = HashMap::new();
-total_cubes.insert("red", 12);
-total_cubes.insert("green", 13);
-total_cubes.insert("blue", 14);
-
 fn main() {
-    for line in read_to_string("puzzle_input.txt").unwrap().lines() {
-        let mut parts = line.split(":");
+    let mut total_cubes = HashMap::new();
+    total_cubes.insert("red", 12);
+    total_cubes.insert("green", 13);
+    total_cubes.insert("blue", 14);
+
+    let mut games_sum = 0;
+
+    'game: for game in read_to_string("puzzle_input.txt").unwrap().lines() {
+        let mut parts = game.split(":");
 
         let game_number = &(parts.next().unwrap())[5..];
         let game_number: i32 = game_number.parse().unwrap();
 
-        println!("*** Game #{game_number}");
-
-        let mut num_cubes = HashMap::new();
-        num_cubes.insert("red", 0);
-        num_cubes.insert("green", 0);
-        num_cubes.insert("blue", 0);
+        println!("\x1b[1m*** Game #{game_number}\x1b[0m");
 
         for pull in parts.next().unwrap().split(";") {
-            println!("{pull:?}");
-            
+            println!("Pull: {pull}");
+            for cubes in pull.trim().split(",") {
+                if let Some((number, color)) = cubes.trim().split_once(" ") {
+                    let number: i32 = number.parse().unwrap();
+                    if number > *total_cubes.get(color).unwrap() {
+                        println!("\x1b[31mInvalid game, {0} > {1} {color} cubes\x1b[0m", number, total_cubes.get(color).unwrap());
+                        continue 'game;
+                    }
+                } else {
+                    panic!("error");
+                };
+            }
         }
-
+        println!("\x1b[32mValid game\x1b[0m 🙂");
+        games_sum += game_number;
     }
+    println!("Sum of valid game IDs: {games_sum}");
 }
