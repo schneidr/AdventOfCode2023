5281d673b94026e8371e84de27444d7572f2e333
Gerald Schneider <gerald@schneidr.de>
2024-07-24 07:01:49 +0200

    Progress on day04-2

--- day04-2/src/main.rs
+++ day04-2/src/main.rs
@@ -1,15 +1,33 @@
 pub mod scratchcard;
-use std::fs::read_to_string;
+use std::{collections::HashMap, fs::read_to_string};
 use crate::scratchcard::scratchcard::Scratchcard;
-
 
 // https://adventofcode.com/2023/day/4#part2
 
 fn main() {
-    let mut my_cards: Vec<Scratchcard> = Vec::new();
+    let mut all_cards: HashMap<i32, Scratchcard> = HashMap::new();
+    let mut my_cards: Vec<i32> = Vec::new();
     for line in read_to_string("scratchcards.txt").unwrap().lines() {
         let card: Scratchcard = Scratchcard::new(&line);
-        my_cards.push(card);
+        let card_number = card.card_number;
+        all_cards.insert(card.card_number, card);
+        my_cards.push(card_number);
     }
-    println!("{my_cards:?}");
+    let mut index = 0;
+    loop {
+        let card_number = my_cards[index];
+        let winning_cards: i32 = all_cards.get(&card_number).unwrap().winning_numbers().len() as i32;
+        println!("{card_number}: {winning_cards:?}");
+        for count in 1..winning_cards+1 {
+            let new_card: usize = (card_number + count + 1) as usize;
+            my_cards.push(my_cards[new_card]);
+        }
+        println!("Progress: {index}/{0} cards", my_cards.len());
+        index += 1;
+        // if index >= 10 {
+        if index >= my_cards.len() {
+            break;
+        }
+    }
+    println!("Total: {0} cards {my_cards:?}", my_cards.len());
 }
--- day04-2/src/scratchcard.rs
+++ day04-2/src/scratchcard.rs
@@ -2,14 +2,13 @@ pub mod scratchcard {
 
     #[derive(Debug)]
     pub struct Scratchcard {
-        card_number: i32,
+        pub card_number: i32,
         winning_numbers: Vec<i32>,
         card_numbers: Vec<i32>
     }
 
     impl Scratchcard {
         pub fn new(input: &str) -> Scratchcard {
-            println!("{input:?}");
             let card_number: i32 = input[5..8].trim().parse().unwrap();
             let winning_numbers: Vec<i32> = input[10..40]
                 .trim()
@@ -21,12 +20,20 @@ pub mod scratchcard {
                 .split_whitespace()
                 .map(|num| num.parse().unwrap())
                 .collect();
-            println!("{winning_numbers:?}");
             Scratchcard {
                 card_number: card_number,
                 winning_numbers: winning_numbers,
                 card_numbers: card_numbers
             }
         }
-    } 
+
+        pub fn winning_numbers(&self) -> Vec<i32> {
+            self.card_numbers
+                .clone()
+                .into_iter()
+                .filter(|item| self.winning_numbers.contains(item))
+                .collect()
+        }
+    }
+
 }
